72b1a0d2d94e293a7450f31902fac937e8c3f351
BNK-ALONG <37407480+BNK-ALONG@users.noreply.github.com>
2019-02-27 21:27:15 +0800

    文件列表——分享、导入、批量删除
    分享、导入、批量删除功能以实现。
    未完成：
    1.下载；需要下载地址，且批量下载的需求可能泡汤
    2.文件列表界面的不够整洁

---
+++ src/api/file.js
@@ -0,0 +1,63 @@
+import axios from '@/libs/api.request'
+
+// 从后台获取文件列表
+export const getFileList = () => {
+  return axios.request({
+    url: '/documents/file_info',
+    method: 'post'
+  })
+}
+
+// 文件的导入或者分享接口
+export const ShareOrImport = ({
+  statusType,
+  file_name_uuid,
+  status
+}) => {
+  if (statusType == '分享') {
+    var ShareStatus = status
+    return axios.request({
+      url: '/documents/share_file',
+      method: 'post',
+      data: {
+        file_name_uuid,
+        ShareStatus
+      }
+    })
+  } else {
+    var NowStatus = status
+    return axios.request({
+      url: '/documents/file_into',
+      method: 'post',
+      data: {
+        file_name_uuid,
+        NowStatus
+      }
+    })
+  }
+}
+
+// 批量下载和删除
+
+export const DownloadOrDelete = ({
+  type,
+  file_name_uuid
+}) => {
+  if (type === '下载') {
+    return axios.request({
+      url: '/documents/download_file',
+      method: 'post',
+      data: {
+        file_name_uuid
+      }
+    })
+  } else {
+    return axios.request({
+      url: '/documents/delete_file',
+      method: 'post',
+      data: {
+        file_name_uuid
+      }
+    })
+  }
+}
--- src/view/documents/file_list.vue
+++ src/view/documents/file_list.vue
@@ -2,25 +2,57 @@
   <div style="width:100%;height:100%;">
     <Upload multiple
             type="drag"
-            action="//jsonplaceholder.typicode.com/posts/">
+            action="/documents/upload_file"
+            name="myfiles"
+            :on-success="uploadSuccess">
       <div style="padding: 20px 0">
         <Icon type="ios-cloud-upload"
               size="52"
               style="color: #3399ff"></Icon>
-        <p>Click or drag files here to upload</p>
+        <p style="font-size:20px;">点击或拖拽文件进行上传</p>
+        <span style="    display: -webkit-inline-box;">
+          <Icon type="ios-checkmark-circle-outline"
+                color="red"
+                size="20" />
+          <p style="font-size:16px;">支持多文件同时上传</p>
+        </span>
+
       </div>
     </Upload>
     <Button @click="handleSelectAll(true)">全 选</Button>
     <Button @click="handleSelectAll(false)">取消全选</Button>
+    <Button type="success"
+            @click="handleDownloadDelete('下载')">下载</Button>
+    <Button type="success"
+            @click="handleDownloadDelete('删除')">删除</Button>
     <Table ref="fileList"
            border
            stripe
            width='686'
            :columns="columns5"
            :data="fileData">
-      <template slot-scope="{ row, index }"
+      <template slot-scope="{row,index}"
+                slot="import">
+        <!-- i-switch开关如果双向绑定的是原数据 开关的动画会卡顿 -->
+        <i-switch v-model="row.NowStatus"
+                  :true-value="trueVal"
+                  :false-value="falseVal"
+                  :disabled="row.extension!=='pdf'"
+                  @on-change="statusChange(index,row.NowStatus,'导入')">
+          <!-- 填坑：想让disabled的值等于表达式的值，则需要绑定disabled的形式:disabled，对vue的双向绑定又多了一份理解。 -->
+          <Icon type="md-checkmark"
+                slot="open"></Icon>
+          <Icon type="md-close"
+                slot="close"></Icon>
+        </i-switch>
+      </template>
+      <template slot-scope="{row,index}"
                 slot="share">
-        <i-switch>
+        <!-- 这里的slot-scope="{row}"只有获取行的数据，不具有修改本行的数据源的能力 -->
+        <i-switch v-model="row.ShareStatus"
+                  :true-value="trueVal"
+                  :false-value="falseVal"
+                  @on-change="statusChange(index,row.ShareStatus,'分享')">
           <Icon type="md-checkmark"
                 slot="open"></Icon>
           <Icon type="md-close"
@@ -28,14 +60,25 @@
         </i-switch>
       </template>
     </Table>
-
+    <Button type="error"
+            @click="handlePrintFileData">打印fileData</Button>
+    <Button type="error"
+            @click="handleSelect">打印已选择的项</Button>
+    <Button type="success"
+            @click="handleDownloadDelete('下载')">下载</Button>
+    <Button type="success"
+            @click="handleDownloadDelete('删除')">删除</Button>
   </div>
 </template>
 <script>
+import { getFileList, ShareOrImport, DownloadOrDelete } from '@/api/file'
+
 export default {
   name: 'file_list',
   data () {
     return {
+      trueVal: 1,
+      falseVal: 0,
       columns5: [
         {
           //type 某一列的类型
@@ -45,55 +88,106 @@ export default {
         },
         {
           type: 'index',
-          width: 80,
+          width: 70,
           //title表头名
           title: '索引',
           //key对对应一下的字段名
-          key: 'index',
-          align: 'center'
+          align: 'center',
+          className: 'tableFontSize'
         },
         {
           title: '文件名',
-          key: 'fileName',
+          key: 'file_name',
           sortable: true,
           align: 'center',
           // maxWidth: 400,
-          // 超出部分显示为省略号
+          // ellipsis超出部分显示为省略号
           ellipsis: true,
+          // tooltip超出一定字数的单元格用气泡提示出来
+          tooltip: true,
+          className: 'tableFontSize'
+
         },
         {
           title: '格式',
-          key: 'fileFormat',
+          key: 'extension',
           align: 'center',
-          maxWidth: 100,
+          maxWidth: 90,
+          className: 'tableFontSize',
           filters: [
+            {
+              label: 'pdf',
+              value: 'pdf'
+            },
             {
               label: 'ppt',
               value: 'ppt'
             },
             {
-              label: 'word',
-              value: 'word'
+              label: 'docx',
+              value: 'docx'
             },
             {
-              label: 'excel',
-              value: 'excel'
+              label: 'xlsx',
+              value: 'xlsx'
+            },
+            {
+              label: 'png',
+              value: 'png'
+            },
+            {
+              label: 'jpg',
+              value: 'jpg'
             }
           ],
           filterMethod (value, row) {
-            return row.address.indexOf(value) > -1;
+            return row.extension === value
           }
         },
         {
-          title: '分享',
-          key: 'share',
-          slot: 'share',
+          title: '导入',
+          key: 'NowStatus',
+          slot: 'import',
           align: 'center',
           maxWidth: 80,
+          className: 'tableFontSize',
+        },
+        {
+          title: '分享',
+          key: 'ShareStatus',
+          slot: 'share',
+          align: 'center',
+          sortable: true,
+          maxWidth: 90,
+          className: 'tableFontSize',
         }
       ],
       fileData: []
     }
+  },
+  mounted () {
+    // 请求后台获取文件列表
+    getFileList().then(res => {
+      const message = res.message
+      const status = res.status
+      if (status === 200) {
+        this.fileData = res.file_info
+        for (let index in this.fileData) {
+          let extension = this.fileData[index].file_name_uuid.split('.').pop()
+          this.fileData[index]['extension'] = extension
+        }
+      } else {
+        this.$Modal.error({
+          title: '文件列表获取失败！',
+          content: '原因：' + message
+        })
+      }
+    }).catch(error => {
+      this.$Modal.error({
+        title: '文件列表获取失败！',
+        content: error
+      })
+    })
   },
   methods: {
     remove (index) {
@@ -101,8 +195,143 @@ export default {
     },
     handleSelectAll (status) {
       this.$refs.fileList.selectAll(status);
+    },
+    // 成功上传文件后的钩子，返回response, file, fileList
+    uploadSuccess (response, file, fileList) {
+      this.$Notice.success({
+        title: file.name + "上传成功！",
+        duration: 3
+      })
+      let fileName = file.name.split('.')
+      this.fileData.push({
+        file_name: fileName[0],
+        extension: fileName[fileName.length - 1],
+        file_name_uuid: response.file_name_uuid,
+        ShareStatus: 0,
+        NowStatus: 0
+      })
+    },
+    // 打印fileData
+    handlePrintFileData () {
+      console.log(this.fileData)
+    },
+    // 打印已选择的项
+    // 通过打印this.$refs.fileList可以知道该组件的所有属性
+    handleSelect () {
+      console.log(this.$refs.fileList.getSelection())
+    },
+    // 分享和取消分享成功或失败、导入和取消导入成功或失败的提示信息
+    share_import_change ({ ResponseStatus, ResponseMessage, statusType, status, file_name }) {
+      if (ResponseStatus === 200) {
+        if (status === 0) {
+          this.$Notice.success({
+            title: '取消' + statusType,
+            desc: "文件——“" + file_name + "”" + '——已取消' + statusType + '!',
+            duration: 6
+          })
+        } else {
+          this.$Notice.success({
+            title: '文件' + statusType,
+            desc: "文件——“" + file_name + "”" + '——' + statusType + '成功！',
+            duration: 6
+          })
+        }
+      } else {
+        if (status === 0) {
+          this.$Notice.error({
+            title: '取消' + statusType + '失败！',
+            desc: ResponseMessage
+            // desc: "文件——“" + file_name + "”" + '——取消' + statusType + '失败！' + '<br>失败原因：' + ResponseMessage
+          })
+        } else {
+          this.$Notice.error({
+            title: '文件' + statusType + '失败！',
+            desc: ResponseMessage
+            // desc: "文件——“" + file_name + "”" + '——' + statusType + '失败！' + '<br>失败原因：' + ResponseMessage
+          })
+        }
+      }
+    },
+    //导入和分享按钮的响应
+    statusChange (index, status, statusType) {
+      console.log(index)
+      let file_name_uuid = this.fileData[index].file_name_uuid
+      let file_name = this.fileData[index].file_name
+      console.log(file_name + statusType + '：' + status)
+      ShareOrImport({
+        statusType: statusType,
+        file_name_uuid: file_name_uuid,
+        status: status
+      }).then(res => {
+        this.share_import_change(
+          {
+            ResponseStatus: res.status,
+            ResponseMessage: res.message,
+            status: status,
+            statusType: statusType,
+            file_name: file_name
+          })
+      }).catch(error => {
+        this.$Modal.error({
+          title: '异常',
+          content: error
+        })
+      })
+    },
+    // 对下载和删除成功或失败的提示消息
+    DownloadDeleteMessage ({ status, type, index, file_name_uuid, file_name }) {
+      if (status === 200) {
+        if (type === '下载') {
+          this.handleSelectAll(false)
+        } else {
+          this.fileData.splice(index, 1)
+          this.$Notice.success({
+            title: "成功删除文件“" + file_name + "”"
+          })
+          this.handleSelectAll(false)
+        }
+      } else {
+        this.$Notice.error({
+          title: type === '下载' ? (file_name + "——下载失败！") : (file_name + "——删除失败！")
+        })
+        this.handleSelectAll(false)
+      }
     }
+    ,
+    // 批量下载和批量删除
+    handleDownloadDelete (type) {
+      let selection = this.$refs.fileList.getSelection()
+      if (selection.length) {
+        for (let file of selection) {
+          // 获取数组对象中值等于selection中的file_name_uuid的索引
+          let index = this.fileData.findIndex(document => document.file_name_uuid === file.file_name_uuid)
 
+          DownloadOrDelete({
+            type: type,
+            file_name_uuid: file.file_name_uuid
+          }).then(res => {
+            this.DownloadDeleteMessage({
+              status: res.status,
+              type: type,
+              index: index,
+              file_name_uuid: file.file_name_uuid,
+              file_name: file.file_name            })
+          }).catch(error => {
+            this.$Modal.error({ title: error })
+          })
+
+        }
+      } else {
+        this.$Modal.warning({
+          title: '请选择要' + type + '的文件！'
+        })
+      }
+    }
   },
 }
 </script>
+<style>
+.tableFontSize {
+  font-size: 16px;
+}
+</style>
